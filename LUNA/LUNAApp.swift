981caacfcd322fcf1cea914878c071b72688c6ad
Ivan Montes <101290628+ivanivanmontes@users.noreply.github.com>
2024-08-12 18:18:03 -0400

    Initial Commit

---
+++ LUNA/LUNAApp.swift
@@ -0,0 +1,17 @@
+//
+//  LUNAApp.swift
+//  LUNA
+//
+//  Created by Ivan Montes on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LUNAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
